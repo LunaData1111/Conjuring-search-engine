da5c44aa6da05743ac60330b4c101e0f33880b65
Christian Luna <christianlunaduenas@gmail.com>
2022-11-28 21:41:59 -0800

    heroku

--- server/server.js
+++ server/server.js
@@ -1,37 +1,77 @@
+// const express = require("express");
+// const { ApolloServer } = require("apollo-server-express");
+// const path = require("path");
+
+// const { typeDefs, resolvers } = require("./schemas");
+// const { authMiddleware } = require("./utils/auth");
+// const db = require("./config/connection");
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware,
+// });
+
+// server.applyMiddleware({ app });
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// // Serve up static assets
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+// db.once("open", () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server running on port ${PORT}!`);
+//     console.log(`http://localhost:${PORT}${server.graphqlPath}`);
+//   });
+// });
+
 const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-
-const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
-
-const PORT = process.env.PORT || 3001;
+// const routes = require('./routes');
+const { authMiddleware } = require("./utils/auth");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
+async function startServer() {
+  await server.start();
+}
+
+startServer();
+
 server.applyMiddleware({ app });
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Serve up static assets
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(dirname, "../client/build")));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(dirname, "../client/build/index.html"));
 });
 
+// app.use(routes);
+
 db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`http://localhost:${PORT}${server.graphqlPath}`);
-  });
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
